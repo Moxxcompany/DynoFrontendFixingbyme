469e737009dd2d8a171888256034ea136f3da1e0
Dharmik Godhani <dharmikgodhani071@gmail.com>
2026-02-06 18:57:52 +0530

    Fixed reste-password UI

--- pages/reset-password.tsx
+++ pages/reset-password.tsx
@@ -12,10 +12,13 @@ import useIsMobile from "@/hooks/useIsMobile";
 import { theme } from "@/styles/theme";
 import PasswordValidation from "@/Components/UI/AuthLayout/PasswordValidation";
 import CustomButton from "@/Components/UI/Buttons";
-import { CardWrapper } from "@/Containers/Login/styled";
+import { AuthContainer, CardWrapper } from "@/Containers/Login/styled";
 import { UserAction } from "@/Redux/Actions";
 import { USER_RESET_PASSWORD } from "@/Redux/Actions/UserAction";
 import { TOAST_SHOW } from "@/Redux/Actions/ToastAction";
+import Image from "next/image";
+import Logo from "@/assets/Images/auth/dynopay-logo.png";
+import LanguageSwitcher from "@/Components/UI/LanguageSwitcher";
 
 const ResetPasswordPage = () => {
   const router = useRouter();
@@ -61,7 +64,7 @@ const ResetPasswordPage = () => {
 
     // Check if confirm password matches (if confirm password has a value)
     if (newPasswordConfirm) {
-      if (value && newPasswordConfirm && value !== newPasswordConfirm && (newPassword === newPasswordConfirm)) {
+      if (value && newPasswordConfirm && value !== newPasswordConfirm) {
         setNewPasswordConfirmError("passwordAndConfirmPasswordShouldBeSame");
       } else {
         setNewPasswordConfirmError("");
@@ -103,7 +106,7 @@ const ResetPasswordPage = () => {
 
   const handleSetNewPassword = () => {
     try {
-      if (newPassword && newPasswordConfirm && token && email) {
+      if (newPassword && newPasswordConfirm && token && email && (newPassword === newPasswordConfirm)) {
         dispatch(
           UserAction(USER_RESET_PASSWORD, {
             token: token,
@@ -141,157 +144,192 @@ const ResetPasswordPage = () => {
   if (!allowed) return null;
 
   return (
-    <CardWrapper sx={{ padding: "30px" }}>
-      <TitleDescription
-        title={t("setNewPassword") || "Set the New Password"}
-        align="left"
-        titleVariant="h2"
-        descriptionVariant="p"
-      />
-      <Box
-        ref={newPasswordFieldRef}
-        sx={{ position: "relative", width: "100%", marginTop: "24px" }}
+    <AuthContainer>
+      <CardWrapper
+        sx={{
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+          padding: isMobile ? "10px 18px" : "8px 27px",
+          height: isMobile ? "49px" : "56px",
+          overflow: "visible",
+        }}
       >
-        <InputField
-          label={t("newPassword")}
-          type={newPasswordShowPassword ? "text" : "password"}
-          value={newPassword}
-          autoComplete="off"
-          onChange={handleNewPasswordChange}
-          onFocus={handleNewPasswordFocus}
-          onBlur={handleNewPasswordBlur}
-          onKeyDown={(e) => {
-            if (
-              e.key === "Enter" &&
-              !userState.loading &&
-              newPassword &&
-              newPasswordConfirm
-            ) {
-              e.preventDefault();
-              handleSetNewPassword();
-            }
+        {/* Logo */}
+        <Image
+          src={Logo}
+          alt="logo"
+          width={isMobile ? 86 : 114}
+          height={isMobile ? 29 : 39}
+          draggable={false}
+          onClick={() => {
+            router.push("/");
           }}
-          placeholder={t("newPasswordPlaceholder")}
-          sideButton={true}
-          sideButtonType="primary"
-          sideButtonIcon={
-            newPasswordShowPassword ? (
-              <VisibilityOffIcon
-                sx={{
-                  color: "#676768",
-                  height: "18px",
-                  width: "16px",
-                }}
-              />
-            ) : (
-              <VisibilityIcon
-                sx={{
-                  color: "#676768",
-                  height: "18px",
-                  width: "16px",
-                }}
-              />
-            )
-          }
-          sideButtonIconWidth={isMobile ? "14px" : "18px"}
-          sideButtonIconHeight={isMobile ? "14px" : "18px"}
-          onSideButtonClick={() => {
-            setNewPasswordShowPassword(!newPasswordShowPassword);
-          }}
-          showPasswordToggle={true}
+          style={{ cursor: "pointer" }}
         />
-        <Box
-          sx={{
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            position: "absolute",
-            ...(isMobile &&
-              theme.breakpoints.down("lg") && {
-              left: "50%",
-              transform: "translateX(-50%)",
-              width: "100%",
-            }),
-            zIndex: 5,
-          }}
-        >
-          <PasswordValidation
-            password={newPassword}
-            anchorEl={newPasswordFieldRef.current}
-            open={newPasswordShowPasswordValidation}
-            onClose={() => setNewPasswordShowPasswordValidation(false)}
-            showOnMobile={newPasswordShowPasswordValidation}
+
+        <Box>
+          <LanguageSwitcher
+            sx={{
+              maxWidth: isMobile ? "78px" : "111px",
+              padding: isMobile ? "7px 10px" : "10px 14px",
+            }}
           />
         </Box>
-      </Box>
-      <Box sx={{ marginTop: "16px" }}>
-        <InputField
-          label={t("newPasswordConfirm")}
-          type={newPasswordConfirmShowPassword ? "text" : "password"}
-          value={newPasswordConfirm}
-          autoComplete="off"
-          onChange={handleNewPasswordConfirmChange}
-          onKeyDown={(e) => {
-            if (
-              e.key === "Enter" &&
-              !userState.loading &&
-              newPassword &&
-              newPasswordConfirm
-            ) {
-              e.preventDefault();
-              handleSetNewPassword();
+      </CardWrapper>
+
+      <CardWrapper sx={{ padding: "30px" }}>
+        <TitleDescription
+          title={t("setNewPassword") || "Set the New Password"}
+          align="left"
+          titleVariant="h2"
+          descriptionVariant="p"
+        />
+        <Box
+          ref={newPasswordFieldRef}
+          sx={{ position: "relative", width: "100%", marginTop: "24px" }}
+        >
+          <InputField
+            label={t("newPassword")}
+            type={newPasswordShowPassword ? "text" : "password"}
+            value={newPassword}
+            autoComplete="off"
+            onChange={handleNewPasswordChange}
+            onFocus={handleNewPasswordFocus}
+            onBlur={handleNewPasswordBlur}
+            onKeyDown={(e) => {
+              if (
+                e.key === "Enter" &&
+                !userState.loading &&
+                newPassword &&
+                newPasswordConfirm
+              ) {
+                e.preventDefault();
+                handleSetNewPassword();
+              }
+            }}
+            placeholder={t("newPasswordPlaceholder")}
+            sideButton={true}
+            sideButtonType="primary"
+            sideButtonIcon={
+              newPasswordShowPassword ? (
+                <VisibilityOffIcon
+                  sx={{
+                    color: "#676768",
+                    height: "18px",
+                    width: "16px",
+                  }}
+                />
+              ) : (
+                <VisibilityIcon
+                  sx={{
+                    color: "#676768",
+                    height: "18px",
+                    width: "16px",
+                  }}
+                />
+              )
             }
-          }}
-          placeholder={t("newPasswordConfirmPlaceholder")}
-          error={!!newPasswordConfirmError}
-          helperText={
-            newPasswordConfirmError
-              ? newPasswordConfirmError.includes(" ")
-                ? newPasswordConfirmError
-                : t(newPasswordConfirmError)
-              : ""
-          }
-          sideButton={true}
-          sideButtonType="primary"
-          sideButtonIcon={
-            newPasswordConfirmShowPassword ? (
-              <VisibilityOffIcon
-                sx={{
-                  color: "#676768",
-                  height: "18px",
-                  width: "16px",
-                }}
-              />
-            ) : (
-              <VisibilityIcon
-                sx={{
-                  color: "#676768",
-                  height: "18px",
-                  width: "16px",
-                }}
-              />
-            )
-          }
-          sideButtonIconWidth={isMobile ? "14px" : "18px"}
-          sideButtonIconHeight={isMobile ? "14px" : "18px"}
-          onSideButtonClick={() => {
-            setNewPasswordConfirmShowPassword(
-              !newPasswordConfirmShowPassword
-            );
-          }}
-          showPasswordToggle={true}
-        />
-      </Box>
-      <Box sx={{ marginTop: "24px" }}>
-        <CustomButton
-          label={t("continue")}
-          variant="primary"
-          size={isMobile ? "small" : "medium"}
-          fullWidth
-          onClick={handleSetNewPassword}
-        />
-      </Box>
-    </CardWrapper>
+            sideButtonIconWidth={isMobile ? "14px" : "18px"}
+            sideButtonIconHeight={isMobile ? "14px" : "18px"}
+            onSideButtonClick={() => {
+              setNewPasswordShowPassword(!newPasswordShowPassword);
+            }}
+            showPasswordToggle={true}
+          />
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              position: "absolute",
+              ...(isMobile &&
+                theme.breakpoints.down("lg") && {
+                left: "50%",
+                transform: "translateX(-50%)",
+                width: "100%",
+              }),
+              zIndex: 5,
+            }}
+          >
+            <PasswordValidation
+              password={newPassword}
+              anchorEl={newPasswordFieldRef.current}
+              open={newPasswordShowPasswordValidation}
+              onClose={() => setNewPasswordShowPasswordValidation(false)}
+              showOnMobile={newPasswordShowPasswordValidation}
+            />
+          </Box>
+        </Box>
+        <Box sx={{ marginTop: "16px" }}>
+          <InputField
+            label={t("newPasswordConfirm")}
+            type={newPasswordConfirmShowPassword ? "text" : "password"}
+            value={newPasswordConfirm}
+            autoComplete="off"
+            onChange={handleNewPasswordConfirmChange}
+            onKeyDown={(e) => {
+              if (
+                e.key === "Enter" &&
+                !userState.loading &&
+                newPassword &&
+                newPasswordConfirm
+              ) {
+                e.preventDefault();
+                handleSetNewPassword();
+              }
+            }}
+            placeholder={t("newPasswordConfirmPlaceholder")}
+            error={!!newPasswordConfirmError}
+            helperText={
+              newPasswordConfirmError
+                ? newPasswordConfirmError.includes(" ")
+                  ? newPasswordConfirmError
+                  : t(newPasswordConfirmError)
+                : ""
+            }
+            sideButton={true}
+            sideButtonType="primary"
+            sideButtonIcon={
+              newPasswordConfirmShowPassword ? (
+                <VisibilityOffIcon
+                  sx={{
+                    color: "#676768",
+                    height: "18px",
+                    width: "16px",
+                  }}
+                />
+              ) : (
+                <VisibilityIcon
+                  sx={{
+                    color: "#676768",
+                    height: "18px",
+                    width: "16px",
+                  }}
+                />
+              )
+            }
+            sideButtonIconWidth={isMobile ? "14px" : "18px"}
+            sideButtonIconHeight={isMobile ? "14px" : "18px"}
+            onSideButtonClick={() => {
+              setNewPasswordConfirmShowPassword(
+                !newPasswordConfirmShowPassword
+              );
+            }}
+            showPasswordToggle={true}
+          />
+        </Box>
+        <Box sx={{ marginTop: "24px" }}>
+          <CustomButton
+            label={t("continue")}
+            variant="primary"
+            size={isMobile ? "small" : "medium"}
+            fullWidth
+            onClick={handleSetNewPassword}
+          />
+        </Box>
+      </CardWrapper>
+    </AuthContainer>
   );
 };
 
